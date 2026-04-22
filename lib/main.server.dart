730aea89530bc34adcb6073a28b6ea673e4817a6
mrleisen <46019233+mrleisen@users.noreply.github.com>
2026-04-22 15:21:13 -0500

    Make favicon red via SVG fill attribute; bump favicon cache

--- lib/main.server.dart
+++ lib/main.server.dart
@@ -21,7 +21,7 @@ void main() {
     head: [
       meta(charset: 'utf-8'),
       meta(name: 'viewport', content: 'width=device-width, initial-scale=1'),
-      link(rel: 'icon', type: 'image/svg+xml', href: 'assets/spine-logo.svg'),
+      link(rel: 'icon', type: 'image/svg+xml', href: 'assets/spine-logo.svg?v=6'),
       link(rel: 'preconnect', href: 'https://fonts.googleapis.com'),
       link(
         rel: 'preconnect',
